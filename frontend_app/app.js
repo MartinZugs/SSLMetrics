7b5282f426144b0e8972d3eb86f878efd313d78d
Jonathan <warkentin.jon@gmail.com>
2020-02-10 18:00:12 -0600

    get rid of electron app; change dependencies; run chart.js instance in node route

--- frontend_app/app.js
+++ frontend_app/app.js
@@ -1,68 +1,97 @@
 const axios = require("axios");
 const fs = require('fs')
 const Path = require('path')
-const csv = require('csv-parser');
-
 var express = require("express");
 var app = express();
 
-let indexResponse = "Index response not yet retrieved from backend"
+app.set('view engine', 'ejs');
+
 let data = "";
-const path = Path.resolve(__dirname, 'all_data.csv')
-const writer = fs.createWriteStream(path)
 const url = "http://192.168.99.100:5000/"
 
-app.get("/", (req, res, next) => {
-    res.send("Hello, Frontend!");
-});
+// app.get("/data", (req, res, next) => {
+//     console.log(data)
+//     res.send(data)
+// });
 
-app.get("/beindex", (req, res, next) => {
-    res.send(indexResponse);
-});
-
-app.get("/data", (req, res, next) => {
-    console.log(data)
-    res.send(data)
-});
-
-async function readCsv() {
-    await fs.createReadStream('all_data.csv')
-        .pipe(csv())
-        .on('data', (row) => {
-            data += JSON.stringify(row)
-            // console.log(row)
-        })
-        .on('end', () => {
-            console.log('CSV file successfully processed');
-        });
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
 
-async function getData(url) {
-    const response = await axios({
-        url,
-        method: 'GET',
-        responseType: 'stream'
+app.get('/chart', function (req, res) {
+    let fileName = 'SSLMetrics_historical.db'
+    console.log("Database filename:")
+    console.log(fileName);
+
+    let rawData = {}
+    let headers = [];
+    let dates = [];
+
+    // Read in DB
+    var knex = require("knex")({
+        client: "sqlite3",
+        connection: {
+            filename: "./database/" + fileName
+        }
     })
-    response.data.pipe(writer)
-    return new Promise((resolve, reject) => {
-        writer.on('finish', resolve)
-        writer.on('error', reject)
-        readCsv();
-    })
-}
 
-const getIndex = async url => {
-    axios.get(url)
-        .then((response) => {
-            indexResponse = response.data;
-            console.log(indexResponse);
-        }, (error) => {
-            console.log(error);
+    // Get all rows from master (all metrics compiled) table in DB
+    let result = knex.from("MASTER").select("*")
+    result.then(function (rows) {
+        // Get all column names in headers array
+        headers = Object.keys(rows[0])
+
+        // Initialize an empty array in the rawData object for each metric
+        for (var i = 0; i < headers.length; i++) {
+            rawData[headers[i]] = []
+        }
+
+        // Ppopulate all metrics arrays (effectively columns of the table) one row at a time
+        for (var j = 0; j < rows.length; j++) {
+            for (var k = 0; k < headers.length; k++) {
+                rawData[headers[k]].push(rows[j][headers[k]])
+            }
+        }
+
+        // Remove dates from rawData object
+        dates = rawData['date']
+        delete rawData['date']
+
+        console.log("Dates:")
+        console.log(dates)
+        console.log()
+        console.log("Raw Data:")
+        console.log(rawData)
+
+        // Create chart dataset object array from rawData
+        let chartDatasets = [];
+        for (metric in rawData) {
+            let randomColor = getRandomColor()
+            chartDatasets.push({
+                label: metric,
+                data: rawData[metric].reverse(),
+                backgroundColor: randomColor,
+                borderColor: randomColor,
+                fill: false,
+            });
+        }
+
+        console.log("Chart Datasets in app.js:")
+        console.log(chartDatasets)
+        console.log(JSON.stringify(dates))
+        res.render('chart', {
+            greeting: 'howdy',
+            dates: JSON.stringify(dates),
+            chartDatasets: JSON.stringify(chartDatasets)
         });
-}
+    });
+})
 
 app.listen(3000, '0.0.0.0', () => {
     console.log("Server running on port 3000");
-    getIndex(url)
-    getData(url + 'data');
 });
